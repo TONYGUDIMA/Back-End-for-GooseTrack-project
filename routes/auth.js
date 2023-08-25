37b87195115f6e5eb1b3d5d8b2586421c7081750
TONYGUDIMA <gudima.anton@yahoo.com>
2023-08-25 21:41:31 +0300

    Validation Transfer to helpers

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = new express.Router();
-const { userRegistration, userLogin } = require('../controllers');
-const { userSignUpValidation, userLoginValidation } = require("../models/joiValidation/JoiUserValidation");
+const {
+  userRegistration,
+  userLogin,
+} = require("../controllers");
+const {
+  userSignUpValidation,
+  userLoginValidation,
+} = require("../helpers/joiValidation/JoiUserValidation");
 
+router.post(
+  "/register",
+  userSignUpValidation,
+  userRegistration
+);
 
-
-router.post('/register', userSignUpValidation, userRegistration);
-
-router.post('/login',  userLoginValidation, userLogin);
-
+router.post(
+  "/login",
+  userLoginValidation,
+  userLogin
+);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = new express.Router();
-const { userLogout, currentUserInfo, updateUserInfo} = require('../controllers');
-const authMiddleware = require('../middlewares/authMiddlewares');
-const { userInfoValidation } = require("../models/joiValidation/JoiUserValidation");
+const {
+  userLogout,
+  currentUserInfo,
+  updateUserInfo,
+} = require("../controllers");
+const authMiddleware = require("../middlewares/authMiddlewares");
+const {
+  userInfoValidation,
+} = require("../helpers/joiValidation/JoiUserValidation");
 
 router.use(authMiddleware);
 
-router.post('/logout', userLogout);
+router.post("/logout", userLogout);
 
-router.get('/current', currentUserInfo);
+router.get("/current", currentUserInfo);
 
-router.patch('/info', userInfoValidation, updateUserInfo)
-
-
-
+router.patch(
+  "/info",
+  userInfoValidation,
+  updateUserInfo
+);
 
 module.exports = router;
-
