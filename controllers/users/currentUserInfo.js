dff2bfb858605e6469e077da79a46db19d083361
TONYGUDIMA <gudima.anton@yahoo.com>
2023-08-28 12:43:18 +0300

    Update currentUserInfo.js

--- controllers/users/currentUserInfo.js
+++ controllers/users/currentUserInfo.js
@@ -1,3 +1,31 @@
 module.exports = async (req, res) => {
-  res.status(200).json(req.user);
+  const {
+    _id,
+    name,
+    email,
+    phone,
+    birthday,
+    skype,
+    avatarUrl,
+  } = req.user;
+
+  res.status(200).json({
+    email,
+    _id,
+    avatarUrl,
+    phone,
+    skype,
+    name,
+    birthday,
+  });
 };
+
+// {
+//   "email": "user@example.com",
+//   "_id": "string",
+//   "avatarURL": "string",
+//   "phone": "string",
+//   "skype": "string",
+//   "name": "string",
+//   "birthday": "string"
+// }
