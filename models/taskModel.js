14fca49d76a5766e0c215b7791263327bbe64bbb
TONYGUDIMA <gudima.anton@yahoo.com>
2023-08-30 21:27:58 +0300

    Update taskModel.js

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,50 +1,51 @@
 const { model, Schema } = require("mongoose");
 const handleMongooseError = require("../helpers/handleMongooseError");
 
-const timeRegexp = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/; 
+const timeRegexp =
+  /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
 
-const taskSchema = new Schema({
+const taskSchema = new Schema(
+  {
     title: {
-    type: String,
-       minlength: 3,
-        maxlength: 250,
+      type: String,
+      minlength: 3,
+      maxlength: 250,
       required: [true, "Title is required"],
     },
-        start: {
-        type: String,
-          match: timeRegexp,
-         required: true,
-         default: "09:00",
+    start: {
+      type: String,
+      match: timeRegexp,
+      required: true,
+      default: "09:00",
     },
     end: {
-        type: String,
-      match: timeRegexp,
-         required: true,
-        default: "09:30",
-    },
-  date: Date,
-     priority: {
       type: String,
-       enum: ["low", "medium", "high"] 
+      match: timeRegexp,
+      required: true,
+      default: "09:30",
+    },
+    date: String,
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
     },
     category: {
       type: String,
-      enum: ["to-do", "in-progress", "done"]
-      
+      enum: ["to-do", "in-progress", "done"],
     },
     owner: {
       type: Schema.Types.ObjectId,
-        ref: "user",
-        required: true,
+      ref: "user",
+      required: true,
     },
-},
-    { versionKey: false, timestamps: true }
-)
+  },
+  { versionKey: false, timestamps: true }
+);
 
 taskSchema.post("save", handleMongooseError);
 
 const Task = model("task", taskSchema);
 
 module.exports = {
-  Task
+  Task,
 };
