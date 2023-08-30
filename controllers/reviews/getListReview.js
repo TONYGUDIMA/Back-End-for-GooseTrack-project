3c47fd74c27c2d87688a24cc7e9d660eef5fb2d5
TONYGUDIMA <gudima.anton@yahoo.com>
2023-08-30 18:05:35 +0300

    Fix populate query

--- controllers/reviews/getListReview.js
+++ controllers/reviews/getListReview.js
@@ -3,8 +3,9 @@ const {
 } = require("../../models/reviewModel");
 
 module.exports = async (req, res) => {
-  const result = await Review.find().populate(
-    "owner"
-  );
+  const result = await Review.find().populate({
+    path: "owner",
+    select: ["name", "avatarUrl"],
+  });
   res.json(result);
 };
