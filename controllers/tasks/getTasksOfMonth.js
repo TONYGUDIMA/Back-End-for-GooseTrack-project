f6bb6d5b005401a2e32541bdfe045cb83c9feb1d
TONYGUDIMA <gudima.anton@yahoo.com>
2023-09-04 00:05:08 +0300

    getTaskFix

--- controllers/tasks/getTasksOfMonth.js
+++ controllers/tasks/getTasksOfMonth.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
   const { month, year } = req.query;
   const startOfMonth = new Date(
     year,
-    month - 1,
+    month,
     1
   ).toLocaleDateString("fr-CA");
   const endOfMonth = new Date(
